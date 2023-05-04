1c42f3c638572221420aea54c913cc0b9007e417
sandy-blaze <sandyblaze911@gmail.com>
2023-05-04 07:47:05 +0530

    upload

--- routes/upload.js
+++ routes/upload.js
@@ -1,14 +1,29 @@
 import express from "express"
-import s3Uploadv2 from "../utility_modules/fileHandler.js"
+// import s3Uploadv2 from "../utility_modules/fileHandler.js"
 import dotenv from "dotenv"
 import StudentController from "../controllers/student.js"
 import multer from "multer"
+import { initializeApp } from "firebase/app"
+import { getStorage, ref, getDownloadURL, uploadBytesResumable } from "firebase/storage"
+
 
 const studentController = StudentController()
 
 dotenv.config()
 const app = express.Router()
 
+const firebaseConfig = {
+    apiKey: process.env.FIRE_BASE_API_KEY,
+    authDomain: process.env.FIRE_BASE_AUTH_DOMAIN,
+    projectId: process.env.FIRE_BASE_PROJECT_ID,
+    storageBucket: process.env.FIRE_BASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIRE_BASE_MESSAGE_SENDER,
+    appId: process.env.FIRE_BASE_APP_ID,
+    measurementId: process.env.FIRE_BASE_MEASUREMENT_ID
+}
+
+initializeApp(firebaseConfig)
+const firebaseStorage = getStorage()
 const storage = multer.memoryStorage()
 const uploadStorage = multer({
     storage: storage,
@@ -21,8 +36,11 @@ app.route('/exam-fee-receipt')
     .post(uploadStorage.array('file_to_upload'), async (req, res) => {
         try {
             const { challana } = req.body
-            const result = await s3Uploadv2(req.files, [`${challana}-exam-fee-receipt`])
-            res.status(200).json({ status: "success", result })
+            const metedata = { contentType: req.file.mimetype }
+            const storageRef = ref(firebaseStorage, `uploads/${challana}-exam-fee-receipt`, metedata)
+            const snapshot = await uploadBytesResumable(storageRef, req.file.buffer)
+            const downloadURL = await getDownloadURL(snapshot.ref)
+            res.status(200).json({ status: "success", result: downloadURL })
         } catch (err) {
             res.status(200).json({ errno: 500 })
         }
@@ -32,35 +50,45 @@ app.route('/revaluation-receipt')
     .post(uploadStorage.array('file_to_upload'), async (req, res) => {
         try {
             const { DU_number } = req.body
-            const result = await s3Uploadv2(req.files, [`${DU_number}-revaluation-receipt`])
-            res.status(200).json({ status: "success", result })
+            const metedata = { contentType: req.file.mimetype }
+            const storageRef = ref(firebaseStorage, `uploads/${DU_number}-revaluation-receipt`, metedata)
+            const snapshot = await uploadBytesResumable(storageRef, req.file.buffer)
+            const downloadURL = await getDownloadURL(snapshot.ref)
+            res.status(200).json({ status: "success", result: downloadURL })
         } catch (err) {
             res.status(200).json({ errno: 500 })
         }
     })
 
 app.route('/certificate-receipt')
-    .post(uploadStorage.array('file_to_upload'), async (req, res) => {
+    .post(uploadStorage.single('file_to_upload'), async (req, res) => {
         try {
             const { DU_number } = req.body
-            const result = await s3Uploadv2(req.files, [`${DU_number}-certificate-receipt`])
-            res.status(200).json({ status: "success", result })
+            const metedata = { contentType: req.file.mimetype }
+            const storageRef = ref(firebaseStorage, `uploads/${DU_number}-certificate-receipt`, metedata)
+            const snapshot = await uploadBytesResumable(storageRef, req.file.buffer)
+            const downloadURL = await getDownloadURL(snapshot.ref)
+            res.status(200).json({ status: "success", result: downloadURL })
         } catch (err) {
             res.status(200).json({ errno: 500 })
         }
     })
 
 app.route('/profile-picture')
-    .post(uploadStorage.array('file_to_upload'), async (req, res) => {
+    .post(uploadStorage.single('file_to_upload'), async (req, res) => {
         try {
             const { roll } = req.body
-            const result = await s3Uploadv2(req.files, [`${roll}-profile-picture`])
-            await studentController.updateProfile({ roll: roll, picture: result[0].Location })
-            res.status(200).json({ status: "success", result })
+            const metedata = { contentType: req.file.mimetype }
+            const storageRef = ref(firebaseStorage, `uploads/${roll}-profile-picture`, metedata)
+            const snapshot = await uploadBytesResumable(storageRef, req.file.buffer)
+            const downloadURL = await getDownloadURL(snapshot.ref)
+            await studentController.updateProfile({ roll: roll, picture: downloadURL }, { new: true })
+            res.status(200).json({ status: "success", result: downloadURL })
         } catch (err) {
             res.status(200).json({ errno: 500 })
         }
     })
+
 
 const fileUpload = app
 export default fileUpload
--- utility_modules/fileHandler.js
+++ utility_modules/fileHandler.js
@@ -40,3 +40,26 @@ export default async function s3Uploadv2(files, keys) {
 //     console.log('upload', 3)
 //     return s3Client.send(new PutObjectCommand(params[0]))
 // }
+
+
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCrlHgLkpJVZb-ssdeBctBEnQMI5-IcvMM",
+    authDomain: "jntuk-db.firebaseapp.com",
+    projectId: "jntuk-db",
+    storageBucket: "jntuk-db.appspot.com",
+    messagingSenderId: "445267723993",
+    appId: "1:445267723993:web:af0e11a9ba623af48060c8",
+    measurementId: "G-2RV0S08Y0B"
+}
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig)
+// const analytics = getAnalytics(app)
